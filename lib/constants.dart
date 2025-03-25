dcb70522f4cc92eb618b6a5389fdeabf2dcfa8d6
ZeyadRaafat <zeyadraafat65@gmail.com>
2025-03-25 12:46:31 +0200

    Adding assets

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
 
-const gif1='assets/animation/white.json';
-const loading='assets/animation/loadinga.json';
-const blossom='assets/images/blossom.jpg';
-const buttercup='assets/images/buttercup.jpg';
-const bubbels='assets/images/bubbles.jpg';
 const blossomcolor=Color(0xfff383a6);
 const bubbelscolor=Color(0xff8db7e9);
 const buttercupcolor=Color(0xff89ca7a);
--- lib/view/screens/splash/mainsplash.dart
+++ lib/view/screens/splash/mainsplash.dart
@@ -1,4 +1,5 @@
 import 'package:e_learning/constants.dart';
+import 'package:e_learning/core/utils/assets.dart';
 import 'package:e_learning/view/screens/homePage.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -27,7 +28,7 @@ class _SplashScreenState extends State<SplashScreen> {
           Container(
             alignment: Alignment(0, -1),
             child: Lottie.asset(
-              gif1,
+              AssetsData.gif1,
               width: 350,
               height: 600,
               fit: BoxFit.contain,
@@ -37,7 +38,7 @@ class _SplashScreenState extends State<SplashScreen> {
           Container(
             alignment: Alignment(0, 0),
             child: Lottie.asset(
-              loading,
+              AssetsData.loading,
               width: 100,
               height: 130,
               fit: BoxFit.contain,
